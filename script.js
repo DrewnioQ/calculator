ddcac4ea76598863bfe4a850972cdf7414fbfb8f
DrewnioQ <debski.dev@gmail.com>
2023-03-17 18:09:33 +0100

    Display and refactor input

--- script.js
+++ script.js
@@ -1,18 +1,78 @@
-function add(a, b) {
-  return a + b;
+let equationArr = [];
+
+const buttons = document.querySelectorAll(".button");
+const displayEq = document.getElementById("displayEq");
+const parsedEq = document.getElementById("parsedEq");
+
+buttons.forEach((button) => addButtonEventListener(button));
+
+function addButtonEventListener(button) {
+  if (button.id === "allClearBtn") {
+    button.addEventListener("click", () => clearDisplayEq());
+    return;
+  }
+  if (button.id === "backspaceBtn") {
+    button.addEventListener("click", () => deleteLastSymbol());
+    return;
+  }
+
+  button.addEventListener("click", () =>
+    addSymbolToEquation(button.textContent)
+  );
 }
 
-function subtract(a, b) {
-  return a - b;
+function addSymbolToEquation(symbol) {
+  symbol = symbol.trim();
+
+  const isOperator = operators.some((operator) => operator.symbol === symbol);
+
+  if (isOperator) symbol = ` ${symbol} `;
+
+  equationArr.push(symbol);
+  updateDisplayEq();
 }
 
-function multiply(a, b) {
-  return a * b;
+function deleteLastSymbol() {
+  equationArr.pop();
+  updateDisplayEq();
 }
 
-function divide(a, b) {
-  return a / b;
+function clearDisplayEq() {
+  equationArr.length = 0;
+  parsedEq.textContent = "";
+  displayEq.textContent = "0";
 }
+
+function updateDisplayEq() {
+  const eqString = equationArr.join("");
+  displayEq.textContent = eqString;
+}
+
+const operators = [
+  {
+    symbol: "+",
+    function: (a, b) => a + b,
+  },
+  {
+    symbol: "-",
+    function: (a, b) => a - b,
+  },
+  {
+    symbol: "*",
+    function: (a, b) => a * b,
+  },
+  {
+    symbol: "/",
+    function: (a, b) => a / b,
+  },
+  {
+    symbol: "%",
+    function: (a, b) => a % b,
+  },
+  {
+    symbol: "=",
+  },
+];
 
 function operate(operator, a, b) {
   if (operator === "+") add(a, b);
