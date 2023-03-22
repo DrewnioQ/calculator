2637f28c150d4ec3d24754c12af8c6a6431de7ee
DrewnioQ <debski.dev@gmail.com>
2023-03-22 16:14:35 +0100

    Update deleteLastSymbol(), add callback to EqBtn

--- script.js
+++ script.js
@@ -1,53 +1,8 @@
 let equationArr = [];
 
 const buttons = document.querySelectorAll(".button");
-const displayEq = document.getElementById("displayEq");
-const parsedEq = document.getElementById("parsedEq");
-
-buttons.forEach((button) => addButtonEventListener(button));
-
-function addButtonEventListener(button) {
-  if (button.id === "allClearBtn") {
-    button.addEventListener("click", () => clearDisplayEq());
-    return;
-  }
-  if (button.id === "backspaceBtn") {
-    button.addEventListener("click", () => deleteLastSymbol());
-    return;
-  }
-
-  button.addEventListener("click", () =>
-    addSymbolToEquation(button.textContent)
-  );
-}
-
-function addSymbolToEquation(symbol) {
-  symbol = symbol.trim();
-
-  const isOperator = operators.some((operator) => operator.symbol === symbol);
-
-  if (isOperator) symbol = ` ${symbol} `;
-
-  equationArr.push(symbol);
-  updateDisplayEq();
-}
-
-function deleteLastSymbol() {
-  equationArr.pop();
-  updateDisplayEq();
-}
-
-function clearDisplayEq() {
-  equationArr.length = 0;
-  parsedEq.textContent = "";
-  displayEq.textContent = "0";
-}
-
-function updateDisplayEq() {
-  const eqString = equationArr.join("");
-  displayEq.textContent = eqString;
-}
-
+const display = document.querySelector("#displayEq");
+const parsedEq = document.querySelector("#parsedEq");
 const operators = [
   {
     symbol: "+",
@@ -74,9 +29,60 @@ const operators = [
   },
 ];
 
-function operate(operator, a, b) {
-  if (operator === "+") add(a, b);
-  if (operator === "/") subtract(a, b);
-  if (operator === "*") multiply(a, b);
-  if (operator === "/") divide(a, b);
+buttons.forEach((button) => addButtonEventListener(button));
+
+function addButtonEventListener(button) {
+  if (button.id === "allClearBtn") {
+    button.addEventListener("click", () => clearDisplay());
+    return;
+  }
+  if (button.id === "backspaceBtn") {
+    button.addEventListener("click", () => deleteLastSymbol());
+    return;
+  }
+  if (button.id === "equalsBtn") {
+    button.addEventListener("click", () => evaluate());
+    return;
+  }
+  button.addEventListener("click", () =>
+    addSymbolToEquation(button.textContent)
+  );
+}
+
+function addSymbolToEquation(symbol) {
+  symbol = symbol.trim();
+  symbol = addSpaceIfOperator(symbol);
+  equationArr.push(symbol);
+  updateDisplay();
+}
+
+function deleteLastSymbol() {
+  equationArr.pop();
+  if (equationArr.length === 0) {
+    display.textContent = "0";
+    return;
+  }
+  updateDisplay();
+}
+
+function evaluate() {
+  parsedEq.textContent = `${display.textContent} =`;
+
+  const eqResult = equationArr.map((symbol) => {});
+}
+
+function clearDisplay() {
+  equationArr.length = 0;
+  parsedEq.textContent = "";
+  display.textContent = "0";
+}
+
+function updateDisplay() {
+  display.textContent = equationArr.join("");
+}
+
+function addSpaceIfOperator(symbol) {
+  const isOperator = operators.some((operator) => operator.symbol === symbol);
+  if (isOperator) return (symbol = ` ${symbol} `);
+  return symbol;
 }
